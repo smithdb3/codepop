254e81ec2541a105fd137b9e20626194a1b93d27
Braden Peterson <peterson.braden2@gmail.com>
2026-03-18 12:09:32 -0600

    changed ip address

--- codepop/ip_address.js
+++ codepop/ip_address.js
@@ -1,3 +1,7 @@
-const BASE_URL = 'http://localhost:8000'; // Your backend URL
-
+import { Platform } from 'react-native';
+                                                                                
+const BASE_URL = Platform.OS === 'android'                                    
+  ? 'http://10.0.2.2:8000'                                                    
+  : 'http://localhost:8000';                                                  
+                                                                                                  
 export { BASE_URL };
