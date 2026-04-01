46f9780db7042b2d57d593d736bb00f02b4bd053
Nathan Haight <nathanjohnhaight@gmail.com>
2026-04-01 02:39:50 -0600

    Added a comment

--- codepop_backend/backend/views.py
+++ codepop_backend/backend/views.py
@@ -1015,6 +1015,7 @@ class ScheduleOperations(viewsets.ModelViewSet):
     serializer_class = ScheduleSerializer
     permission_classes = [IsAuthenticated]
     
+    # Returns all the schedules associated with a user
     def get_user_schedules(self, request):
         user_schedules = Schedule.objects.filter(assigned_to=request.user)
         serializer = self.get_serializer(user_schedules, many=True)
